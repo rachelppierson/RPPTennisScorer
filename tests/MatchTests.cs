85db01b0a63dce6326b81733dce6e7805c6215ed
Rachel Pierson <rachel.pierson@serialaffinity.co.uk>
2023-07-06 18:10:52 +0100

    MatchTests class created. Theory test set up. Will add more data from the input file.

---
+++ tests/MatchTests.cs
@@ -0,0 +1,35 @@
+﻿using RPPTennisScorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTests
+{
+    public class MatchTests
+    {
+        readonly Match _matchUnderTest;
+
+        public MatchTests()
+        {
+            _matchUnderTest = new Match();
+        }
+
+        #region Test cases from the 'input' samples, tested invidually
+
+        [Theory]
+        [InlineData("", "0-0")]
+        public void CheckTestSample1(string input, string expectedOutput)
+        {
+            //Act
+            _matchUnderTest.ScoreMatch(input);
+            var _actual = _matchUnderTest.ToString();
+
+            //Assert
+            Assert.Equal(expectedOutput, _actual);
+        }
+
+        #endregion
+    }
+}
